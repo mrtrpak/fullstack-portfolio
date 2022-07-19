6182cf5b39bbcf1740f650a82ce0c2265cca8c3c
Michael Trpak <mrtrpak3@gmail.com>
2022-07-19 15:11:20 -0400

    adding in route for getting soccer standings and needed variables

--- server.js
+++ server.js
@@ -3,13 +3,33 @@ const axios = require('axios'),
  cors = require("cors"),
  path = require('path'),
  PORT = process.env.PORT || 3333;
-
 app = express();
+
+const soccerKey = process.env.soccerKey;
+const { soccerKeyDevelopment } = require('./secret');
 
 app.use(cors());
 
+//serve static files from react
+
 app.get('/', (req,res) => {
   res.send("hello from the server");
+});
+
+app.get('/api/soccer', async (req, res) => {
+  try {
+    const response = await axios({
+      "url": 
+        `https://api.football-data.org/v2/competitions/BL1/standings`,
+      "headers": {
+        "X-Auth-Token": soccerKey || soccerKeyDevelopment,
+        "Content-Type": "application/json"
+      }
+    })
+    res.send(response.data.standings[0].table);
+  } catch (err) {
+    res.send(err);
+  };
 });
 
 app.listen(
