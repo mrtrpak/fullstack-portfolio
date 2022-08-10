7980da7a8c391ed42e18d923fa27d19149a8cb99
Michael Trpak <mrtrpak3@gmail.com>
2022-08-10 16:20:52 -0400

    adding data for different leagues for reference and for key to render based on league.

---
+++ client/src/components/soccer/soccer-key.js
@@ -0,0 +1,39 @@
+const soccerKeyGuide = [
+  {
+    "BL1": {
+      "CL": "The top 4 teams at the end of the season directly qualify for the Champions League of the following year. If a Bundesliga team wins the Champions League or Europa League without finishing in the top 4 they still qualify for the Champions League.",
+      "EL": "The teams that finish 5th and 6th in the standings directly qualify for the Europa League. The DFB Cup winners also qualify. If the Cup winners already qualify due to league position the 7th place team qualifies.",
+      "REL": "The teams that finish in 17th and 18th place are directly relegated to Bundesliga 2. The 16th placed finisher plays the 3rd place finisher of Bundesliga 2 in a 2-legged play-off to determine who plays in the Bundesliga. Each team plays at home and the team that scores more goals over both games overall wins. In case of a draw a 30 minute overtime period is played. If still tied a penalty shootout decides the winner."
+    },
+    "PL": {
+      "CL": "The top four teams in the Premier League directly qualify for the Champions League. If a team outside the top 4 wins the Champions League or Europa League then they qualify directly for the Champions League.",
+      "EL": "The 5th place team qualify directly for the Europa League. The winners of the FA Cup qualify directly as well. The EFL Cup winners qualify for the Europa League playoff stage.",
+      "REL": "The bottom 3 teams are all relegated to English Football League Championship."
+    },
+    "PD": {
+      "CL": "The top 4 teams in La Liga qualify directly for the Champions League. If a team wins the Champions League or Europa League and finishes outside of the top 4 they also qualify.",
+      "EL": "The 5th placed team qualifies directly for the Europa League. The winner of the Copa del Rey also directly qualifies. If the winner of Copa del Rey finishes in the top 5, their spot goes to the 6th place finisher.",
+      "REL": "The 3 bottom teams are all relegated to Segunda Division."
+    },
+    "SA": {
+      "CL": "The top 4 teams directly qualify for the Champions League. If a team outside the top 4 wins the Champions League or Europa League then they qualify directly for the Champions League.",
+      "EL": "The team finishing 5th as well as the Coppa Italia winner qualify directly for the Europa League. If the Coppa Italia winner finishes in the top 5 their their spot goes to the 6th place winner.",
+      "REL": "The 3 botton teams are all relegated to Serie B."
+    },
+    "FL1": {
+      "CL": "The top 2 teams directly qualify for the Champions League and the 3rd place finisher enters in the third qualifying round.",
+      "EL": "The 4th place finisher qualifies directly for the Europa League. The other 2 spots are determined by the winners of the Coupe de France and Coupe de la Ligue. If those winners are in the top 4 spots then the 5th and/or 6th place finishers take their spot.",
+      "REL": "The bottom 2 teams get relegated to Ligue 2."
+    },
+    "DED": {
+      "CL": "The winner of the league qualifies directly to the Champions League. The 2nd place finisher qualifies for the 3rd round of League Path in the Champions League.",
+      "EL": "The KNVB Cup winner qualifies for the Europa League play-off round.",
+      "REL": "The 17th and 18th placed team directly get relegated to Eerste Divisie. The 16th placed team goes into the semi-final against an Eerste Divisie team. The winner of the play-offs plays in the Eredivisie."
+    },
+    "PPL": {
+      "CL": "The top 2 teams directly qualify for the Champions League and the 3rd place finisher qualifies for the playoffs to get into the competition.",
+      "EL": "If the 3rd place finisher doesn't qualify for the Champions League they will be entered into the Europa League. The 4th place finisher and the winner of Taca de Portugal also directly qualify. If the winner of Taca de Portugal qualify for the Champions League their spot is given to the 5th placed team.",
+      "REL": "The last 2 teams are relegated to Liga Portugal 2 and the 16th placed team go into a relegation play-offs."
+    }
+  }
+]
